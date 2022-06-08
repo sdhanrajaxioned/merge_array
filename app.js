1b67f89bf0dcb1bc54738f76c395bc4f9f36952b
sdhanrajaxioned <dhanrajs@axioned.com>
2022-06-08 14:52:23 +0530

    formatted code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let array1 = [1,2,3,4,5],
   array2 = [1,3,7,8,9,8];
 
-let uniqueItems = array1.filter((item) => array2.indexOf(item) == -1);
+let uniqueItems = array1.filter( item => array2.indexOf(item) == -1);
 // let mergedArr = array2.concat(uniqueItems).sort();
 let mergedArr = [...array2, ...uniqueItems].sort();
 
